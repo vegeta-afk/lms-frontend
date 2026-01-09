151cc120fcd1edbd3d6603fb135a1c97a7581fca
Anidh Kaushik <onlinebusiness1504@gmail.com>
2026-01-09 17:44:04 +0530

    Fix login API integration and prepare frontend for deployment

--- src/services/api.js
+++ src/services/api.js
@@ -10,7 +10,7 @@ const api = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
-  timeout: 10000, // 10 second timeout
+  timeout: 120000, // 40 second timeout
 });
 
 // Request interceptor to add auth token
